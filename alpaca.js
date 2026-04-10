262cd3fac55566d9f6671687cd1e1e4fa6cccbd4
wilidiriba20 <157874486+wilidiriba20@users.noreply.github.com>
2026-04-10 12:52:07 +0300

    feat: add initial alpaca customization options and button functionality

---
+++ alpaca.js
@@ -0,0 +1,64 @@
+let hair = ["bang", "curly", "default", "elegant", "fancy", "quiff", "short"];
+let ears = ["default", "tilt-backward", "tilt-forward"];
+let eyes = ["angry", "default", "naughty", "panda", "smart", "star"];
+let leg = [
+  "bubble-tea",
+  "cookie",
+  "default",
+  "game-console",
+  "tilt-backward",
+  "tilt-forward",
+];
+let mouth = ["astonished", "default", "eating", "laugh", "tongue"];
+let neck = ["bend-backward", "bend-forward", "default", "thick"];
+let accessories = ["earings", "flower", "glasses", "headphone"];
+let background = [
+  "blue50",
+  "blue60",
+  "blue70",
+  "darkblue30",
+  "darkblue50",
+  "darkblue70",
+  "green50",
+  "green60",
+  "green70",
+  "grey40",
+  "grey70",
+  "grey80",
+  "red50",
+  "red60",
+  "red70",
+  "yellow50",
+  "yellow60",
+  "yellow70",
+];
+
+let buttonGroup = document.getElementsByClassName("button-group");
+let buttons = Array.from(document.getElementsByTagName("button")); // FIX
+
+function buttonscreate(type) {
+  let arr = [];
+
+  if (type === "hair") arr = hair;
+  else if (type === "ears") arr = ears;
+  else if (type === "eyes") arr = eyes;
+  else if (type === "leg") arr = leg;
+  else if (type === "mouth") arr = mouth;
+  else if (type === "neck") arr = neck;
+  else if (type === "accessories") arr = accessories;
+  else if (type === "background") arr = background;
+
+  buttonGroup[1].innerHTML = "";
+
+  arr.forEach((item) => {
+    let button = document.createElement("button");
+    button.innerText = item;
+    buttonGroup[1].appendChild(button);
+  });
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    buttonscreate(event.target.id);
+  });
+});
